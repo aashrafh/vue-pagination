51d623cc7320987193b18ae585041d45357eeb0d
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-08-08 01:42:31 +0200

    pagination

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,37 @@
   <div>
     <div class="container p-4">
       <div class="row">
-        <div class="col-md-4" v-for="article in articles" :key="article.id">
+        <div class="col-md-4" v-for="article in displayedArticles" :key="article.id">
           <div class="card mb-2">
             <img v-bind:src="article.imageURL" class="card-img-top" />
             <div class="card-body">
-              <h5>{{article.title}}</h5>
-              <p>{{article.description.slice(0, 100)}}</p>
+              <h5 class="capitalize">{{ article.title }}</h5>
+              <p>{{ article.description.slice(0, 120) }}...</p>
             </div>
           </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="btn-group col-md-2 offset-md-5">
+          <button
+            type="button"
+            class="btn btn-sm btn-outline-secondary"
+            v-if="page != 1"
+            @click="page--"
+          >Previous</button>
+          <button
+            type="button"
+            class="btn btn-sm btn-outline-secondary"
+            v-for="pageNumber in pages.slice(page-1, page+5)"
+            :key="pageNumber"
+            @click="page = pageNumber"
+          >{{pageNumber}}</button>
+          <button
+            type="button"
+            class="btn btn-sm btn-outline-secondary"
+            v-if="page < pages.length"
+            @click="page++"
+          >Next</button>
         </div>
       </div>
     </div>
@@ -34,6 +57,27 @@ export default {
     getArticles: async function() {
       const res = await this.axios.get(`${this.baseURL}/articles`);
       this.articles = res.data.articles;
+    },
+    setArticles: function() {
+      const numberOfPages = Math.ceil(this.articles.length / this.perPage);
+      for (let i = 1; i <= numberOfPages; i++) this.pages.push(i);
+    },
+    paginate: function(articles) {
+      const page = this.page;
+      const perPage = this.perPage;
+      const from = page * perPage - perPage;
+      const to = page * perPage;
+      return articles.slice(from, to);
+    }
+  },
+  computed: {
+    displayedArticles: function() {
+      return this.paginate(this.articles);
+    }
+  },
+  watch: {
+    articles() {
+      this.setArticles();
     }
   }
 };
